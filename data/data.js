e6b6a4669ed5dcd669d9c581dde6850a8d9323f6
pankajgouraw <pgouraw@gmail.com>
2019-08-23 16:48:41 +0530

    functionality update

--- data/data.js
+++ data/data.js
@@ -1,22 +1,37 @@
 
-const bgColor = '#a9a9a9';
-const borderColor = '10px solid #36ae87';
+const bgColor = '#000';
+const borderColor = '10px solid #07887c';
 const header = 'Activity';
 const alphabate = 'B';
+
+const beepSound = 'audio/beep.mp3';
+const wellDone = 'audio/wellDone.mp3';
+const tryAgain = 'audio/tryAgain.mp3';
+
 const data = [
 	{
 		'question' : ["a","l","l"],
 		'corrAns'  : 'b',
 		'sugg1'    : 'c',
 		'sugg2'    : 'd',
+		'audio'    : 'audio/aclip.mp3',
+		'img'      : 'img/ball.png'
 	},
-	
-		{
-		'question' : 'Which animal in this video12?',
-		'video'    : 'videos/dog.mp4',
-		'corrAns'  : 'Dog',
-		'sugg1'    : 'Cat',
-		'sugg2'    : 'Elephant',
-		'sugg3'    : 'Zebra'
-	}
+	{
+		'question' : ["a","t"],
+		'corrAns'  : 'b',
+		'sugg1'    : 'e',
+		'sugg2'    : 'r',
+		'audio'    : 'audio/aclip.mp3',
+		'img'      : 'img/bat.png'
+	},
+    {
+		'question' : ["a","l","l","o","o","n"],
+		'corrAns'  : 'b',
+		'sugg1'    : 'g',
+		'sugg2'    : 'y',
+		'audio'    : 'audio/aclip.mp3',
+		'img'      : 'img/ball.png'
+	},
+
 ];
--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 $(function() {
+
+  let index = 0;
+
+
   $("#activityText").text(header);
   $('.wrapper').css({'outline':borderColor});
   $('body').css({'background':bgColor});
   $('#LetterText').text(alphabate);
+
+  // url value
+  let url = window.location.href;
+  if(url.indexOf('?') > 0){
+    let params = new URLSearchParams(url.substring(1));
+    index = parseInt(params.get('qno'));
+    console.log("available....");
+  }else{
+    console.log("not available...");
+     let mainAudio = new Audio(data[0].audio);
+     mainAudio.play();
+  }
   
   // function for drag and drop
-
   function dragDrop(){
   $('.dragg').draggable({
         revert: 'invalid'
@@ -23,23 +38,107 @@ $(function() {
                 console.log("correct")
                 $(this).addClass('right');
                 $('.happy').slideUp().fadeIn();
+                let audioData = new Audio(wellDone);
+                audioData.play();
+                  if(index == data.length-1){
+                    $('#next').hide();
+                  }else{
+                    $('#next').fadeIn();
+                  }
             } else{
                 console.log("incorrect");
                 $(this).addClass('wrong');
                 $('.sad').slideUp().fadeIn();
+                let audioData = new Audio(tryAgain);
+                audioData.play();
             }
         }
   }); 
 
   }  //end here drag and drop 
 
+
+
+  let questionHtml = '';
+
+  // function to load the data
+  function loadData(){
+      let question = data[index].question;
+
+      // to append the questions
+      $.each(question, function( index, value ) {
+        let x = `<div class="suggData"><span>${value}</span></div>`;
+        questionHtml = questionHtml +x;
+      });
+      console.log(question);
+      $('.dropSuggestions').html(questionHtml);
+      
+      // to play audio
+      let mainAudio = new Audio(data[index].audio);
+      mainAudio.play();
+      // to append the suggestions
+      let suggHtml = `<div id="match" class="suggData dragg"><span>${data[index].corrAns}</span></div>
+                     <div class="suggData suggData1 dragg"><span>${data[index].sugg1}</span></div>
+                     <div class="suggData suggData2 dragg"><span>${data[index].sugg2}</span></div>`;
+      $('.suggestionContainer').html(suggHtml);
+
+      // to append the img
+      $('.imgContainer').html(`<img id="image" src="${data[index].img}" class="animated zoomIn slow">`);
+  }  // end function to load the data
+
+
+  // function to generate random index
+  function randomsuggestion(){
+   $('#match').css({  'order': Math.floor((Math.random() * 3) + 1)});
+   $('.suggData1').css({  'order': Math.floor((Math.random() * 3) + 1)});
+   $('.suggData2').css({  'order': Math.floor((Math.random() * 3) + 1)});
+  }  // end function to generate random index
+
+  loadData();
   dragDrop();
-
-
-  // Try Again
+  randomsuggestion();
 
   $('#tryAgain').click(function(){
        location.reload();
   })
+
+  $('#next').click(function(){
+    console.log("i am the next one...");
+    let audioData = new Audio(beepSound);
+    audioData.play();
+    index++;
+    let url2 = window.location.pathname;
+    var newurl = url2 + `?data=all&qno=${index}`;
+
+    setTimeout(function(){
+      window.location.href = newurl;
+    },500)
+    
+  })
+
+  $('#prev').click(function(){
+    console.log("i am the next one...");
+    let audioData = new Audio(beepSound);
+    audioData.play();
+    index--;
+    let url2 = window.location.pathname;
+    var newurl = url2 + `?data=all&qno=${index}`;
+    setTimeout(function(){
+      window.location.href = newurl;
+    },500)
+  })
+
+  if(index > 0){
+    $('#prev').fadeIn();
+  }else{
+     $('#prev').hide();
+  }
+
+  if(index > 0){
+    $('#next').fadeIn();
+  }
+  if(index == data.length-1){
+    $('#next').hide();
+  }
 
 });
